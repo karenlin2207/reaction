926322f3792e0da08aeeb5ee5dbe2fec76387577
karenlin2207 <ddrenlin@gmail.com>
2016-10-14 10:57:33 +0800

    allpay

--- imports/plugins/core/orders/client/templates/list/ordersList.js
+++ imports/plugins/core/orders/client/templates/list/ordersList.js
@@ -23,6 +23,45 @@ Template.dashboardOrdersList.helpers({
       limit: 25
     });
   },
+  allpaycheckoutform(){
+      var allpaycheckout;
+      var strUrl = location.search;
+      var getPara, ParaVal;
+      var aryPara = [];
+      if (strUrl.indexOf("?") != -1) {
+        var getSearch = strUrl.split("?");
+        getPara = getSearch[1].split("&");
+        for (i = 0; i < getPara.length; i++) {
+          ParaVal = getPara[i].split("=");
+          aryPara.push(ParaVal[1]);
+          aryPara[ParaVal[1]] = ParaVal[2];
+        }
+      }
+      var order = Orders.findOne({ cartId: aryPara[0] });
+      var items = order.items;
+      var Items = new Array();
+      items.forEach(function(data){
+        var item = { name : data.title, price: parseInt(data.variants.price) ,currency : "NTD", quantity: data.quantity};
+        Items.push(item);
+      })
+      var date = new Date();
+      let cartInfo={
+        MerchantTradeNo : aryPara[0],
+        MerchantTradeDate: moment(date).format("YYYY/MM/DD HH:mm:ss"),
+        TotalAmount: parseInt(order.billing[0].paymentMethod.amount),
+        TradeDesc: "test",
+        Items:  Items,
+        ReturnURL: "https://ec2-52-43-22-203.us-west-2.compute.amazonaws.com/receive",
+        ChoosePayment: "ALL"
+      };
+
+      allpaycheckout = Meteor.call("checkoutMac", cartInfo,function(err, result){
+        allpaycheckout = result.html;
+        Session.set("allpaycheckout", result.html);
+      });
+    
+      return Session.get('allpaycheckout');
+  },  
   orderAge() {
     return moment(this.createdAt).fromNow();
   },
@@ -32,5 +71,12 @@ Template.dashboardOrdersList.helpers({
   shopName() {
     const shop = Shops.findOne(this.shopId);
     return shop !== null ? shop.name : void 0;
+  }
+});
+
+Template.dashboardOrdersList.events({
+  'click #allpaybutton': function(event){
+    var form = document.getElementById("_allpayForm");
+    form.submit();
   }
 });
